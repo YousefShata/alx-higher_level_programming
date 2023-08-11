484bff3112994d1ea4f9e9e2714a78ac0ce2075d
YousefShata <yossef_shata_a@yahoo.com>
2023-08-11 16:25:27 -0700

    another fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
 
     a = 1
     b = 2
 
-    add = add_0.add(a, b)
-
-    print(f'{a} + {b} = {add}')
+    print(f'{a} + {b} = {add(a, b)}')

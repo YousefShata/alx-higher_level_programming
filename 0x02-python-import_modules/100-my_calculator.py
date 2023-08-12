ea782cb5cd57321dc0214b9219e1d1114f08a8dc
YousefShata <yossef_shata_a@yahoo.com>
2023-08-11 18:35:16 -0700

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (len(argv) != 4):
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit (1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == "+":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)

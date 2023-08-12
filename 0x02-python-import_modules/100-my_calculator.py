6f6305a42dc3315b01909cad77b302d4ba5ac64d
YousefShata <yossef_shata_a@yahoo.com>
2023-08-11 18:37:26 -0700

    quick fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ from sys import argv
 if __name__ == "__main__":
     if (len(argv) != 4):
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit (1)
+        exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])

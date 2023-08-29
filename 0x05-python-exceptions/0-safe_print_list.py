495cfa2ee06d4a3778fce3c70e765719bae718df
YousefShata <yossef_shata_a@yahoo.com>
2023-08-28 18:05:35 -0700

    pyton betty

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while x > 0:
-            print("{}".format(my_list[i]),end="")
-            i+=1
-            x-=1
+            print("{}".format(my_list[i]), end="")
+            i += 1
+            x -= 1
         print("")
     except IndexError:
         print("")
-    return i            
+    return i

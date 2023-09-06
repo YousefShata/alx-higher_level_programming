36a6c3f44d34094c54902b3429631032e470b47f
YousefShata <yossef_shata_a@yahoo.com>
2023-09-06 02:02:39 -0700

    betty 2.0

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 Module that prevents the user from dynamically creating new instance attributes
 """
 
 
 class LockedClass:
     """
-    a clas with no objects or attributes 
+    a class with no objects or attributes
     that only accept the attriube named "first_name"
     """
     __slots__ = "first_name"

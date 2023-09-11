50a5ce27b565f9fb184a4b03c69461357e8c11ad
YousefShata <yossef_shata_a@yahoo.com>
2023-09-11 16:11:19 -0700

    area calculation almsot done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module docs
+"""
+
+
+class BaseGeometry(object):
+    """
+    class for BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")

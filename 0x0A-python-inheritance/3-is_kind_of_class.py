bf0c7b57f1595c0886dfda5d6c5d22cc2353d992
YousefShata <yossef_shata_a@yahoo.com>
2023-09-11 16:03:48 -0700

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module Doc
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function to check if object inharet from classh
+    """
+
+    return isinstance(obj, a_class)

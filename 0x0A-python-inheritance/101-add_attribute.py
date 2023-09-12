4d571656e6ceb34ca9ac930f2b24b84241d68e9c
YousefShata <yossef_shata_a@yahoo.com>
2023-09-11 17:46:51 -0700

    add attribute dynamically

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module doc
+"""
+
+
+def add_attribute(a_class, att_name, att_value):
+    """
+    function too add att dynamically
+    """
+
+    if not hasattr(a_class, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(a_class, att_name, att_value)

07e6221ec3a3315f8b3cbe2d52213ad9b291a727
YousefShata <yossef_shata_a@yahoo.com>
2023-09-12 13:08:41 -0700

    why

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+readfile module
+"""
+
+
+def read_file(filename=""):
+    """
+    read file
+    """
+    with open(filename, encoding=("utf-8")) as Myfile:
+        Myfile.read()

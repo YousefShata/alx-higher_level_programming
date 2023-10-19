249e341a825ee7b9745e811dc897ec873209c027
YousefShata <yossef_shata_a@yahoo.com>
2023-10-18 18:33:30 -0700

    typo

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,5 +4,5 @@ JOIN tv_show_genres
 ON tv_show_genres.genres_id = tv_genres.id
 JOIN tv_shows
 ON tv_shows.id = tv_show_genres.genres_id
-WHERE tv_shows.name = 'Dexter'
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name

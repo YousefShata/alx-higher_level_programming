5d6d8ba2d7784463b278b7b0ef807f9796e0ba18
YousefShata <yossef_shata_a@yahoo.com>
2023-10-18 17:05:33 -0700

    CAPS

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- CALIFORNIA
 SELECT id, name FROM cities WHERE state_id = (
-SELECT id FROM states where name = "California") ORDER BY id;
+SELECT id FROM states WHERE name = "California") ORDER BY id;

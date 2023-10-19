a8bf3ad3be6a6f8dd5b98b45488389f67b6abef7
YousefShata <yossef_shata_a@yahoo.com>
2023-10-18 18:38:45 -0700

    hotfix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- my genres
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres
-ON tv_show_genres.genres_id = tv_genres.id
+ON tv_genres.id = tv_show_genres.genres_id
 JOIN tv_shows
-ON tv_shows.id = tv_show_genres.genres_id
+ON tv_show_genres.genres_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name

9628537b71bb01e66eb22752af6b7758f8a69c4b
YousefShata <yossef_shata_a@yahoo.com>
2023-11-14 19:02:32 -0800

    betty 3.0

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,14 +18,14 @@ module.exports = class Rectangle {
     }
   }
 
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
   double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
-  }
-
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
   }
 };

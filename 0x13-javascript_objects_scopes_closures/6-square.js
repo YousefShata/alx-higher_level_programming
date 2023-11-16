e0484c440b5b189fb53f8b6ba25a703766f1bbc2
YousefShata <yossef_shata_a@yahoo.com>
2023-11-15 18:50:26 -0800

    betty 3.0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,12 +5,12 @@ module.exports = class Square extends Square2 {
     super(size, size);
   }
 
-    charPrint (c = 'X') {
-    let row = '';
-    for (let x = 0; x < this.height; x++) {
-      for (let y = 0; y < this.width; y++) {
-        row = row + c;
-      }
+  charPrint (c = 'X') {
+  let row = '';
+  for (let x = 0; x < this.height; x++) {
+    for (let y = 0; y < this.width; y++) {
+      row = row + c;
+    }
       console.log(row);
       row = '';
     }

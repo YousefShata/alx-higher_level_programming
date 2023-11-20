37a98d248c76a0459c956c648a12e5f5ea7d1273
YousefShata <yossef_shata_a@yahoo.com>
2023-11-19 19:00:36 -0800

    filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+ORM model
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Main function
+    """
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+    search = argv[4]
+    try:
+
+        conn = MySQLdb.connect(host="localhost",
+                               port=3306, user=user,
+                               passwd=password, db=db)
+        try:
+            cur = conn.cursor()
+            quary = "SELECT id, name FROM states"
+            quary += "WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
+                    search.strip("")
+                    )
+            cur.execute(quary)
+
+            rows = cur.fetchall()
+
+            for row in rows:
+                print(row)
+        finally:
+            cur.close()
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()

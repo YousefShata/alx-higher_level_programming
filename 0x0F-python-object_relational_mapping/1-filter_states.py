a3ff533a856923b51950be2cdc332fbe6255a478
YousefShata <yossef_shata_a@yahoo.com>
2023-11-19 18:43:42 -0800

    N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+ORM model
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Main function
+    """
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    try:
+
+        conn = MySQLdb.connect(host="localhost",
+                               port=3306, user=user,
+                               passwd=password, db=db)
+        cur = conn.cursor()
+
+        quary = """
+            SELECT id, name FROM states WHERE name LIKE 'N%'
+            ORDER BY id ASC
+        """
+        cur.execute(quary)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cur.close()
+        conn.close()
+    except e:
+        pass
+
+
+if __name__ == "__main__":
+    main()

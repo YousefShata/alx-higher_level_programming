f2bc61f011f10a24383c680d0af39c215a6c75c4
YousefShata <yossef_shata_a@yahoo.com>
2023-11-21 01:55:25 -0800

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+ORM model
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Main function
+    """
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+    search = argv[4]
+    try:
+
+        conn = MySQLdb.connect(host="localhost",
+                               port=3306, user=user,
+                               passwd=password, db=db)
+        try:
+            cur = conn.cursor()
+            quary = "SELECT id, name FROM states"
+            quary += " WHERE name LIKE BINARY %s ORDER BY id ASC"
+
+
+            cur.execute(quary, (f'{search%}',))
+
+            rows = cur.fetchall()
+
+            for row in rows:
+                print(row)
+        finally:
+            cur.close()
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()

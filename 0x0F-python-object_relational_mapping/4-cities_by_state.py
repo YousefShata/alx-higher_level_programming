fb66f12a2bac860c1a780920270766c9a1ccb55e
YousefShata <yossef_shata_a@yahoo.com>
2023-11-21 02:08:07 -0800

    all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+ORM model
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Main function
+    """
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    try:
+
+        conn = MySQLdb.connect(host="localhost",
+                               port=3306, user=user,
+                               passwd=password, db=db)
+        try:
+            cur = conn.cursor()
+            quary = "SELECT * FROM cities"
+
+            cur.execute(quary)
+
+            rows = cur.fetchall()
+
+            for row in rows:
+                print(row)
+        finally:
+            cur.close()
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()

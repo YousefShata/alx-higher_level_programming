588d612fb95b6236666d9d1d7b8df88634270c41
YousefShata <yossef_shata_a@yahoo.com>
2024-01-10 18:12:46 -0800

    square update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+charPrint (c = 'X') {
+    let row = '';
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        row = row + c;
+      }
+      console.log(row);
+      row = '';
+    }
+  }
+};

f1dcc33340e9e9f37c7217594979ee822a1f30bc
YousefShata <yossef_shata_a@yahoo.com>
2024-01-10 18:08:01 -0800

    error fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,9 @@
 const numArguments = process.argv.length;
 
 if (numArguments < 3) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (numArguments === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
+}

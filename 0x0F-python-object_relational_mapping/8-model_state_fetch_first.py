78bc67e349d3f38c64613c2176b1fdb216322197
YousefShata <yossef_shata_a@yahoo.com>
2024-01-14 17:25:12 -0800

    first fetch

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+fetch all
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+def main():
+    """
+    main Doc
+    """
+
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+    engine = create_engine(
+               'mysql+mysqldb://{}:{}@localhost/{}'
+               .format(user,
+                       password,
+                       db),
+               pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
+
+
+if __name__ == "__main__":
+    main()

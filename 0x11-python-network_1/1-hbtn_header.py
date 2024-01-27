9124b10e91eba439a3e339d997bf455190f43d0e
YousefShata <yossef_shata_a@yahoo.com>
2024-01-26 18:27:15 -0800

    trying something

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Module Doc
+"""
 from sys import argv
 from urllib import request
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    id = response.headers.get("X-Request-Id")
-    print(f"{id}")
+    with urllib.request.urlopen(url) as response:
+        id = response.headers.get("X-Request-Id")
+        print(f"{id}")

03eb50fc2802d8e1236ac574c90537449ee8f1d7
YousefShata <yossef_shata_a@yahoo.com>
2024-01-26 18:31:44 -0800

    some enhancment

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+Module Doc
+"""
 from sys import argv
-from urllib import request, parse
+import urllib.request
 
-url = argv[2]
+if __name__ == "__main__":
+    url = argv[2]
 
-data = urllib.parse.urlencode({'email': url})
-data = data.encode('ascii')
-req = urllib.request.Request(argv[1], data)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page.decode('utf-8'))
+    data = urllib.parse.urlencode({'email': url[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))

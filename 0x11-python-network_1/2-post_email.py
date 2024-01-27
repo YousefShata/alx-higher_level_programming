dd1f5b392b3ca191e5ab7ea97b55029bb445d6ba
YousefShata <yossef_shata_a@yahoo.com>
2024-01-26 18:38:29 -0800

    what is going on ?

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
+from sys import argv
+from urllib import request, parse
 
 url = argv[2]
 
-data = urllib.parse.urlencode({'email': url[2]})
-data = data.encode('ascii')
-req = urllib.request.Request(argv[1], data)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page.decode('utf-8'))
+if __name__ == "__main__":
+
+    data = urllib.parse.urlencode({'email': url[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))

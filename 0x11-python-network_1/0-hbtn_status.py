56b372b0f01ec6bd1e887f4f845d48fb0bc1c551
YousefShata <yossef_shata_a@yahoo.com>
2024-01-26 16:45:35 -0800

    refix output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
-    print("Body request:")
+    print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 contect: {}".format(html.decode("utf-8")))

b7c6201482a799e84cff1af8be5fd4c60ddd1a31
YousefShata <yossef_shata_a@yahoo.com>
2024-01-30 18:14:37 -0800

    fix post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ url = argv[2]
 
 if __name__ == "__main__":
 
-    data = urllib.parse.urlencode({'email': url[2]})
+    data = urllib.parse.urlencode({'email': url})
     data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:

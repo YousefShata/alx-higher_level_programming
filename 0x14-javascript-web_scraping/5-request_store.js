026edfd4b80bf35d8c2e4f83f1de17ea7221f42b
YousefShata <yossef_shata_a@yahoo.com>
2024-02-20 19:00:21 -0800

    sync data

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  try {
+    fs.writeFileSync(file, body, { encoding: 'utf8' });
+  } catch (error) {
+    console.error(error);
+  }
+});

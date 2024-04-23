27a4e014441c706e6d885435e897ba8e183efc5f
YousefShata <yossef_shata_a@yahoo.com>
2024-04-23 13:48:41 -0700

    starwars actor counts

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const actorId = '18';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  let totalMovies = 0;
+  const movieData = JSON.parse(body);
+  if (response.statusCode === 200) {
+    movieData.results.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${actorId}/`)) {
+        totalMovies++;
+      }
+    });
+  }
+  console.log(totalMovies);
+});

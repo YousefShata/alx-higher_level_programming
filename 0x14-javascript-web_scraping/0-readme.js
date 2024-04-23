41743d4fb5dedee8bd0b1fe189d68b9aaeb95684
YousefShata <yossef_shata_a@yahoo.com>
2024-04-23 11:38:43 -0700

    betty x.0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file_name = process.argv[2];
+const fileName = process.argv[2];
 
-fs.readFile(file_name, 'utf-8', (err, data) => {
-	if (err)
-	{
-		console.log(err);
-	}
-	if (data)
-	{
-		console.log(data);
-	}
-})
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  if (data) {
+    console.log(data);
+  }
+});
